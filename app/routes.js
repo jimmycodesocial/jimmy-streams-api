3a89c30058cd7c4fb6f685ade74698c9be4f4a35
Alberto Menendez Romero <alberto.menendez@globant.com>
2016-04-14 22:53:34 -0300

    Endpoints.

--- app/routes.js
+++ app/routes.js
@@ -9,26 +9,26 @@ let activities = require('./controllers/activities');
 
 // Streams Collection
 // Create new streams
-router.post('streams', streams.create);
+router.post('/streams', streams.create);
 
 // Subscriptions Collection
 // Subscribe the stream to another stream
-router.post('streams/:name/subscriptions', subscriptions.create);
+router.post('/streams/:name/subscriptions', subscriptions.create);
 
 // Get the list of subscriptions from the stream
-router.get('streams/:name/subscriptions', subscriptions.get);
+router.get('/streams/:name/subscriptions', subscriptions.get);
 
 // Modify how a stream is subscribed to another stream
-router.put('streams/:name/subscriptions/:stream', subscriptions.put);
+router.put('/streams/:name/subscriptions/:stream', subscriptions.put);
 
 // Remove the subscription existing between two streams
-router.delete('streams/:name/subscriptions/:stream', subscriptions.delete);
+router.delete('/streams/:name/subscriptions/:stream', subscriptions.delete);
 
 // Activities Collection
 // Receive new activities
-router.post('activities', activities.create);
+router.post('/activities', activities.create);
 
 // Get the list of activities that were saved in the stream
-router.get('streams/:name/activities', activities.get);
+router.get('/streams/:name/activities', activities.get);
 
 module.exports = router;
