0adc5563e8a40d04f9adc5f83d852c5f0105b6cc
Alberto Menendez Romero <alberto.menendez@globant.com>
2016-04-15 00:36:07 -0300

    Extract and format the error list in a Joi validation error

---
+++ app/formatter/joiErrorSchemaToJsonApi.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* global module */
+
+/**
+ * Extract and format the error list in a Joi validation error
+ * @see: https://github.com/hapijs/joi/blob/v8.0.5/API.md#errors
+ *
+ * @param schema_error joi.validate error result
+ */
+module.exports = (schema_error) => {
+  if (!schema_error || !schema_error.isJoi) {
+    return schema_error;
+  }
+
+  let errors = {};
+  schema_error.details.forEach((error) => {
+    errors[error.path] = error.message;
+  });
+
+  return errors;
+};
