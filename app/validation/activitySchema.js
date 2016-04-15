4c82cb93e9910a4d0e1102fbcaf9ddbaf368842b
Alberto Menendez Romero <alberto.menendez@globant.com>
2016-04-15 00:58:41 -0300

    Doc

--- app/validation/activitySchema.js
+++ app/validation/activitySchema.js
@@ -8,10 +8,34 @@ let Joi = require('joi');
  * @see: http://activitystrea.ms/specs/json/1.0/#activity
  */
 module.exports = Joi.object().keys({
+  // The date and time at which the activity was published.
+  // An activity MUST contain a published property.
   published: Joi.date().iso().required(),
+
+  // Describes the entity that performed the activity.
+  // An activity MUST contain one actor property whose value is a single Object.
   actor: Joi.object().keys({
     id: Joi.string().required(),
     objectType: Joi.string().required()
   }).with('id', 'objectType'),
-  verb: Joi.string().required()
+
+  // Identifies the action that the activity describes.
+  // An activity SHOULD contain a verb property whose value is a JSON String that is non-empty.
+  verb: Joi.string().required(),
+
+  // Describes the primary object of the activity.
+  // An activity SHOULD contain an object property whose value is a single Object.
+  object: Joi.object().keys({
+    id: Joi.string().required(),
+    objectType: Joi.string().required()
+  }).with('id', 'objectType'),
+
+  // Describes the target of the activity.
+  // The precise meaning of the activity's target is dependent on the activities verb,
+  // but will often be the object the English preposition "to".
+  // An activity MAY contain a target property whose value is a single Object.
+  target: Joi.object().keys({
+    id: Joi.string().required(),
+    objectType: Joi.string().required()
+  }).with('id', 'objectType')
 }).without('target', 'to');
