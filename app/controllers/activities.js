8fc8f6a7df9f4309217e5b73c0bc130efd29197b
Alberto Menendez Romero <alberto.menendez@globant.com>
2016-04-16 00:28:19 -0300

    Endpoint implementation. POST /activities. Validate input and accept activities by sending them to an SQS queue.

--- app/controllers/activities.js
+++ app/controllers/activities.js
@@ -2,9 +2,11 @@
 
 /* global require, module */
 
+let logger = require('config').get('app').logger;
 let Joi = require('joi');
 let activitySchemaValidation = require('../validation/activitySchema');
 let joiErrorSchemaToJsonApi = require('../formatter/joiErrorSchemaToJsonApi');
+let poolSQS = require('../pool/sqs');
 
 /**
  * Receive new activities
@@ -14,23 +16,43 @@ let joiErrorSchemaToJsonApi = require('../formatter/joiErrorSchemaToJsonApi');
  * @param next
  */
 module.exports.create = (req, res, next) => {
+  logger.info('Receive new activities', {activity: req.body});
+
   // Validate json
   Joi.validate(req.body, activitySchemaValidation, (err, value) => {
     if (err) {
+      let errors = joiErrorSchemaToJsonApi(err);
+      logger.warn('Validation problem', {'errors': errors});
+
       // Format the output to be JSON-API compatible
       // @see: http://jsonapi.org/format/#errors
       return res.status(400).json({
         status: 400,
-        code: 'E_ACTIVITY_ACCEPTANCE',
+        code: 'E_ACTIVITY_VALIDATION',
         title: 'Validation error',
         details: 'The activity cannot be accepted due to validation errors.',
         meta: {
-          errors: joiErrorSchemaToJsonApi(err)
+          errors: errors
         }
       });
     }
 
-    return res.json({'status': 'OK'});
+    // Send the activity to the pool to be analyzed
+    poolSQS.sendActivity(req.body, (err) => {
+      if (err) {
+        logger.error('Error accepting an activity', { 'error': err });
+
+        return res.status(400).json({
+          status: 400,
+          code: 'E_ACTIVITY_ACCEPTANCE',
+          title: 'Activity not accepted',
+          details: 'The activity cannot be accepted due to internal errors while sending it to the pool.'
+        });
+      }
+
+      // The activity was accepted correctly
+      return res.status(202).json({});
+    });
   });
 };
 
