8347d73c4321fc47fc05e42721ead731c9832cb9
Alberto Menendez Romero <alberto.menendez@globant.com>
2016-04-17 18:00:21 -0300

    Creating and removing streams.

--- app/controllers/streams.js
+++ app/controllers/streams.js
@@ -1,35 +1,109 @@
 'use strict';
 
-/* global require, module */
-
-let Joi = require('joi');
-let streamSchemaValidation = require('../validation/streamSchema');
-let joiErrorSchemaToJsonApi = require('../formatter/joiErrorSchemaToJsonApi');
+import Joi from 'joi';
+import config from 'config';
+import streamSchemaValidation from '../validation/streamSchema';
+import joiErrorSchemaToJsonApi from './../jsonApi/formatter/joiErrorSchemaToJsonApi';
+import {createStream, removeStream} from '../graph';
+import StreamSerializer from '../jsonApi/serializer/stream';
+let logger = config.get('app').logger;
 
 /**
  * Create new streams
  *
  * @param req
  * @param res
- * @param next
  */
-module.exports.create = (req, res, next) => {
+export const create = (req, res) => {
+  logger.info('Receive new stream', {stream: req.body});
+
   // Validate json
   Joi.validate(req.body, streamSchemaValidation, (err, value) => {
     if (err) {
+      let errors = joiErrorSchemaToJsonApi(err);
+      logger.warn('Validation error', {'errors': errors});
+
       // Format the output to be JSON-API compatible
       // @see: http://jsonapi.org/format/#errors
       return res.status(400).json({
         status: 400,
-        code: 'E_STREAM_CREATION',
+        code: 'E_STREAM_VALIDATION',
         title: 'Validation error',
         details: 'The stream cannot be created due to validation errors.',
         meta: {
-          errors: joiErrorSchemaToJsonApi(err)
+          errors: errors
         }
       });
     }
 
-    return res.json({'status': 'OK'});
+    // Create the stream node in the graph
+    logger.debug('Create the stream node in the graph');
+
+    return createStream(value, (err) => {
+      // Error occurred while creating the node.
+      if (err) {
+        logger.error('Error creating the stream', {'error': err});
+
+        // Format the output to be JSON-API compatible
+        // @see: http://jsonapi.org/format/#errors
+        return res.status(500).json({
+          status: 500,
+          code: 'E_STREAM_CREATION',
+          title: 'Error creating the stream',
+          details: 'The stream cannot be created due to internal errors.',
+          meta: {
+            error: err.message || 'no message'
+          }
+        });
+      }
+
+      // Acknowledge the creation
+      return res.status(201).json(StreamSerializer.serialize({
+        // Use the name as Id
+        data: {...value, id: value.name}
+      }));
+    });
   });
 };
+
+/**
+ * Remove existing streams
+ *
+ * @param req
+ * @param res
+ */
+export const remove = (req, res) => {
+  let stream = req.params.name;
+
+  // Remove the node from the graph
+  logger.info('Remove an stream', {stream: stream});
+
+  return removeStream(stream, (err) => {
+    logger.error('Error removing the stream', {'stream': stream});
+
+    if (err) {
+      // Format the output to be JSON-API compatible
+      // @see: http://jsonapi.org/format/#errors
+      return res.status(500).json({
+        status: 500,
+        code: 'E_STREAM_DELETION',
+        title: 'Error removing the stream',
+        details: 'The stream cannot be removed due to internal errors.',
+        meta: {
+          error: err.message || 'no message'
+        }
+      });
+    }
+
+    // No representation
+    return res.status(204).json({});
+  });
+};
+
+/**
+ * Expose controllers
+ */
+export default {
+  create: create,
+  remove: remove
+}
