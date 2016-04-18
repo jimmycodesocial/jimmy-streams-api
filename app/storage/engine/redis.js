2b8e843e783aa3c3d66dc24414dd615cfdf154f9
Alberto Menendez Romero <alberto.menendez@globant.com>
2016-04-17 23:42:21 -0300

    Redis Storage Engine

---
+++ app/storage/engine/redis.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Redis client
+ * @type {RedisClient}
+ */
+let redis = null;
+
+/**
+ * Set a connected redis client
+ *
+ * @param {RedisClient} client Redis client for communication
+ * @param {function}    done   Callback to notify
+ */
+export const setClient = (client, done) => {
+  redis = client;
+
+  return done(null);
+};
+
+/**
+ * Search activities with pagination.
+ *
+ * @param {string}   stream Search activities from this stream
+ * @param {integer}  page   Pagination
+ * @param {integer}  limit  How many activities per page?
+ * @param {function} done   Callback to notify results
+ * @returns {*}
+ */
+export const paginate = (stream, page, limit, done) => {
+  page = page || 1;
+  limit = limit || 25;
+
+  let startAt = (page - 1) * limit;
+  let stopAt = startAt + limit - 1;
+
+  return redis.zrange(stream, startAt, stopAt, (err, results) => {
+    return done(err, results);
+  })
+};
+
+/**
+ * Expose API
+ */
+export default {
+  setClient: setClient,
+  paginate: paginate
+};
