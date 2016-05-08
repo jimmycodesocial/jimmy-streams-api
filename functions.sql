990a599f7219896630d3f0a3570485a5cd77be52
Joel Macias <joel.macias@globant.com>
2016-05-08 14:09:23 -0300

    Sql function to find subscriptions of an stream

---
+++ functions.sql
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- findSubscribers Function
+-- Returns the "id" property of all the streams "subscribed_to" another stream
+-- PARAMETERS:
+--   * starter: The Stream Id
+--   * offset: Pagination control, use with SKIP
+--   * quantity: Pagination control, use with LIMIT
+-----------------------------------------------------------------------------------------------------------------------
+DELETE FROM OFunction where name = 'findSubscribers';
+
+CREATE FUNCTION findSubscribers
+"SELECT expand(in('SUBSCRIBED_TO').id) from (SELECT FROM Stream WHERE id = :starter) SKIP :offset LIMIT :quantity;"
+PARAMETERS [starter, offset, quantity] IDEMPOTENT false LANGUAGE sql
