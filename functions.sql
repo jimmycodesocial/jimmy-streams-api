6b345e0dc25dbc3f3c8f76147970ed4c81056730
Joel Macías Roque <joelmcs6@gmail.com>
2016-05-18 00:54:48 -0300

    set notification parameters in docs

--- functions.sql
+++ functions.sql
@@ -3,6 +3,7 @@
 -- Returns the "id" property of all the streams "subscribed_to" another stream
 -- PARAMETERS:
 --   * starter: The Stream Id
+--   * notification: The edge have to be from notification or not
 --   * offset: Pagination control, use with SKIP
 --   * quantity: Pagination control, use with LIMIT
 -----------------------------------------------------------------------------------------------------------------------
