1dbf5ab954bd2ac69f74139b8f3bc90dade9006e
Joel Macías Roque <joelmcs6@gmail.com>
2016-05-18 02:36:27 -0300

    fix bug with notification parameter

--- functions.sql
+++ functions.sql
@@ -3,14 +3,14 @@
 -- Returns the "id" property of all the streams "subscribed_to" another stream
 -- PARAMETERS:
 --   * starter: The Stream Id
---   * notification: The edge have to be from notification or not
+--   * notification: The edge notify property value.
 --   * offset: Pagination control, use with SKIP
 --   * quantity: Pagination control, use with LIMIT
 -----------------------------------------------------------------------------------------------------------------------
 DELETE FROM OFunction where name = 'findSubscriptions';
 
 CREATE FUNCTION findSubscriptions
-"SELECT EXPAND(out) FROM SUBSCRIBED_TO WHERE in.id = :starter AND notify = :notification skip :offset limit :quantity;"
+"SELECT EXPAND(out) FROM SUBSCRIBED_TO WHERE in.id = :starter AND (notify = :notification OR :notification = false) skip :offset limit :quantity;"
 PARAMETERS [starter, notification, offset, quantity] IDEMPOTENT true LANGUAGE sql
 
 SELECT EXPAND(out) FROM SUBSCRIBED_TO WHERE in.id = :starter AND notify = :notification skip :offset limit :quantity
